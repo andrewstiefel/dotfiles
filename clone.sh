af7e930825306713f02811a3c421f92825b10a2e
Andrew Stiefel <andrew@andrewstiefel.com>
2024-06-02 13:54:07 -0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -5,4 +5,7 @@ echo "Cloning repositories..."
 CODE=$HOME/Code
 
 # Personal
+git clone git@github.com:andrewstiefel/1password-netlify-function.git $CODE/1password-netlify-function
 git clone git@github.com:andrewstiefel/andrewstiefel.com.git $CODE/andrewstiefel.com
+git clone git@github.com:andrewstiefel/buttondown-netlify-function.git $CODE/buttondown-netlify-function
+git clone git@github.com:andrewstiefel/eleventy-blog.git $CODE/eleventy-blog
